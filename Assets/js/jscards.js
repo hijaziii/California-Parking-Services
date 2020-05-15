27ea45aa789c9ffba0c84f79ea66ebd1275e9873
Kathleen Yoannon <k.yoannon@gmail.com>
2020-05-14 19:27:31 -0700

    -

--- Assets/js/jscards.js
+++ Assets/js/jscards.js
@@ -27,7 +27,7 @@ $(document).ready(function () {
 
     var canvas = document.getElementById("canvas");
     // var ctx = canvas.getContext("2d");
-     var radius = canvas.height / 2;
+    //  var radius = canvas.height / 2;
     ctx.translate(radius, radius);
      radius = radius * 0.90
     setInterval(drawClock, 1000);
